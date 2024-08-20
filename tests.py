20a82469fab1ecc39ed9e06838380b999680a89a
Dana <chertilas@mail.ru>
2024-08-20 11:45:11 +0300

    All tests set_book_genre

--- tests.py
+++ tests.py
@@ -20,6 +20,8 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
+
+
     @pytest.fixture(autouse=True)
     def class_object(self):
         self.collector = BooksCollector()
@@ -36,7 +38,6 @@ class TestBooksCollector:
 
         assert len(self.collector.get_books_genre()) == 1
 
-
     @pytest.mark.parametrize("book_name",['','o' * 41])
     def test_add_new_book_invalid_book_name_not_add(self,book_name):
         self.collector.add_new_book(book_name)
@@ -44,6 +45,32 @@ class TestBooksCollector:
         assert len(self.collector.get_books_genre()) == 0 and book_name not in self.collector.get_books_genre()
 
 
+
+    def test_set_book_genre_existing_book_set_genre(self):
+        book_name = 'Руководство по ремонту ВАЗ 2017'
+        self.collector.add_new_book(book_name)
+
+        genre = 'Фантастика'
+        self.collector.set_book_genre(book_name, genre)
+
+        assert self.collector.get_book_genre(book_name) == genre
+
+    def test_set_book_genre_non_existent_book_no_genre(self):
+        book_name = 'Новая книга'
+        genre = 'Фантастика'
+
+        self.collector.set_book_genre(book_name, genre)
+
+        assert book_name not in self.collector.get_books_genre()
+
+    @pytest.mark.parametrize("invalid_genre", ['Триллер', 'Кино'])
+    def test_set_book_genre_invalid_gener_false(self,invalid_genre):
+        book_name = 'Звонок'
+        self.collector.add_new_book(book_name)
+
+        self.collector.set_book_genre(book_name,invalid_genre)
+
+        assert self.collector.get_book_genre(book_name) == ''
 
 
 
