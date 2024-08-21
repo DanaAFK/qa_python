7182820ac8e0651ad83989d2daf2596939375554
Dana <chertilas@mail.ru>
2024-08-21 18:10:16 +0300

    fixed auto fixture, fixed attribute errors

--- conftest.py
+++ conftest.py
@@ -11,12 +11,12 @@ def genre():
 
 @pytest.fixture
 def add_book_with_genre(class_object, book_name, genre):
-    class_object.collector.add_new_book(book_name)
-    class_object.collector.set_book_genre(book_name, genre)
+    class_object.add_new_book(book_name)
+    class_object.set_book_genre(book_name, genre)
 
 @pytest.fixture
 def add_book_to_favorites(class_object, add_book_with_genre, book_name):
-        class_object.collector.add_book_in_favorites(book_name)
+        class_object.add_book_in_favorites(book_name)
 
 
 
